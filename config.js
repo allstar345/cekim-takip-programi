75ec68e71502434a934434612da2f1e86a5fac00
allstar345 <fatihglbdk@gmail.com>
2025-10-27 11:54:44 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// config.js
-export const supabaseUrl = "https://vpxwjehzdbyekpfborbc.supabase.co";
-export const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZweHdqZWh6ZGJ5ZWtwZmJvcmJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc3NDgwMzYsImV4cCI6MjA3MzMyNDAzNn0.nFKMdfFeoGOgjZAcAke4ZeHxAhH2FLLNfMzD-QLQd18";
+// config.js – TEK KAYNAK
+export const supabaseUrl = 'https://vpxwjehzdbyekpfborbc.supabase.co';
+export const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZweHdqZWh6ZGJ5ZWtwZmJvcmJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc3NDgwMzYsImV4cCI6MjA3MzMyNDAzNn0.nFKMdfFeoGOgjZAcAke4ZeHxAhH2FLLNfMzD-QLQd18';
 export const db = supabase.createClient(supabaseUrl, supabaseAnonKey);
